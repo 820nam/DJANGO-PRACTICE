721d4541abd3cbc86178121aa61bc5296c8d226d
LEE GILNAM <gilnam3947@gmail.com>
2024-11-22 10:42:44 +0900

    <Commit Note> 1. index.html Edit 2. list.html Add 3. read.html Add

--- fbv-board/board/views.py
+++ fbv-board/board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render  # render 함수 임포트
+from django.shortcuts import render  # render 함수 임포트: 템플릿을 렌더링하여 HTML로 반환하는 역할
 
 # Create your views here.  # Django가 기본으로 제공하는 주석: 뷰 함수를 여기에 작성하라는 의미
 
@@ -6,3 +6,23 @@ from django.shortcuts import render  # render 함수 임포트
 def index(request):  
     # 요청(request)이 들어오면 'board/index.html' 템플릿을 렌더링하여 응답으로 반환
     return render(request, 'board/index.html')  
+
+from .models import Board  # Board 모델 임포트: 데이터베이스에서 게시글 데이터를 가져오기 위해 사용
+
+# list 함수: 게시판 목록을 렌더링하는 뷰
+def list(request):
+    board_list = Board.objects.all()  # Board 모델의 모든 객체(게시글) 데이터를 가져옴
+    context = {
+        'board_list': board_list,  # 가져온 게시글 데이터를 템플릿에 전달하기 위한 딕셔너리
+    }
+    return render(
+        request,
+        'board/list.html',  # 렌더링할 템플릿 파일 경로
+        context  # 템플릿에 전달할 데이터
+    )
+    
+    
+def read(request, id):
+    board = Board.objects.get(pk=id)
+    board.incrementReadCount() #조회수 count
+    return render(request, 'board/read.html', {'board':board})
